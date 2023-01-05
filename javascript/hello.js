5d9a6aa32cc72a0ea48fe5cf53f7fc67dffeb962
nirvanafan205 <nirvanafan205@gmail.com>
2023-01-04 16:39:02 -0800

    basics overview

---
+++ javascript/hello.js
@@ -0,0 +1,14 @@
+console.log('Hello World');
+
+const myName = "Matthew Garcia";
+console.log(myName);
+
+const firstName = "Roy"; //const doesn't let it be changed
+let ayyyLmao = "Lil Mayo"; //let allows variable to be mutatable
+const lastName = "ROgers";
+
+const sentence = "Hello " + firstName + " " + lastName + "! How are you!?";
+const sentenceWithTemplate = `Hello ${firstName} ${lastName}! How are you!?`;
+
+console.log(sentence);
+console.log(sentenceWithTemplate);
